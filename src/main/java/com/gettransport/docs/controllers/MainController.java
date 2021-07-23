c17b4329cfe31cbc62309b3a2243e2d3c3f3c20e
Volk-d <dsvlk@outlook.com>
2021-07-23 21:58:07 +0300

    created html

--- src/main/java/com/gettransport/docs/controllers/MainController.java
+++ src/main/java/com/gettransport/docs/controllers/MainController.java
@@ -3,15 +3,14 @@ package com.gettransport.docs.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    public String greeting(Model model) {
+        model.addAttribute("title", "Документы");
+        return "home";
     }
 
 }
