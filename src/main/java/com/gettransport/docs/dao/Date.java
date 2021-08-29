a0d3b08b3761ddb0cffd61b4f8ef70efadc6b487
Volk-d <dsvlk@outlook.com>
2021-08-29 22:28:15 +0300

    synchronization of licensed and agent

--- src/main/java/com/gettransport/docs/dao/Date.java
+++ src/main/java/com/gettransport/docs/dao/Date.java
@@ -13,8 +13,29 @@ public class Date  {
 
 
     public static void add(String id,Carriage carriage){
-        new Cleaner(id);
-        mapCarriages.put(id,carriage);
+        Carriage otherCarriage = (Carriage) mapCarriages.get(id);
+        if(otherCarriage != null){
+            if(carriage.getNumber() == null) carriage.setNumber(otherCarriage.getNumber());
+            if(carriage.getShipper_name() == null) carriage.setShipper_name(otherCarriage.getShipper_name());
+            if(carriage.getShipper_signatory() == null) carriage.setShipper_signatory(otherCarriage.getShipper_signatory());
+            if(carriage.getShipper_requisites() == null) carriage.setShipper_requisites(otherCarriage.getShipper_requisites());
+            if(carriage.getCarrier_name() == null) carriage.setCarrier_name(otherCarriage.getCarrier_name());
+            if(carriage.getCarrier_signatory() == null) carriage.setCarrier_signatory(otherCarriage.getCarrier_signatory());
+            if(carriage.getCarrier_requisites() == null) carriage.setCarrier_requisites(otherCarriage.getCarrier_requisites());
+            if(carriage.getPick_up_date() == null) carriage.setPick_up_date(otherCarriage.getPick_up_date());
+            if(carriage.getPick_down_date() == null) carriage.setPick_down_date(otherCarriage.getPick_down_date());
+//                        carriage.setTax(otherCarriage.getLoading_place());
+//            if(carriage.getUnloading_place() == null) carriage;
+//            if(carriage == null) carriage;
+//            if(carriage == null) carriage;
+//            if(carriage == null) carriage;
+//            if(carriage == null) carriage;
+//            if(carriage == null) carriage;
+            mapCarriages.put(id,carriage);
+        } else {
+            new Cleaner(id);
+            mapCarriages.put(id, carriage);
+        }
     }
     public static Carriage get(String id) {
         return (Carriage) mapCarriages.get(id);
